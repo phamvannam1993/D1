d906e6a757bd7f511d57498f6e832d430180d488
Your Name <you@example.com>
2024-09-10 07:46:27 +0700

    fix

---
+++ test/index.spec.ts
@@ -0,0 +1,12 @@
+// test/index.spec.ts
+import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+import { describe, it, expect } from 'vitest';
+import worker from '../src/index';
+
+// For now, you'll need to do something like this to get a correctly-typed
+// `Request` to pass to `worker.fetch()`.
+const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+
+describe('Hello World worker', () => {
+
+});
